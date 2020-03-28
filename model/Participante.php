57757884f686506a595662632b90ff3fd0e9ac0a
crangarita <crangarita@gmail.com>
2020-03-28 17:01:29 -0500

    Se realiza el modelo y las operaciones CRUD para conectarnos a la base de datos

--- model/Participante.php
+++ model/Participante.php
@@ -312,6 +312,80 @@ class Participante {
     }
 
 
+    public function update(Participante $data) {
+
+    	try {
+
+	    	$sql = "UPDATE participante SET documento = ?, nombre = ?, fechanacimiento = ?, email = ?, codigo = ?, clave = ?, foto = ?, tipo = ?, telefono = ?, facebook = ?, twitter = ? WHERE id = ? ";
+
+	    	$this->pdo->prepare($sql)->execute(
+	    		array(
+	    			$data->getDocumento(), 
+	    			$data->getNombre(), 
+	    			$data->getFechanacimiento(), 
+	    			$data->getEmail(), 
+	    			$data->getCodigo(), 
+	    			$data->getClave(), 
+	    			$data->getFoto(), 
+	    			$data->getTipo(), 
+	    			$data->getTelefono(), 
+	    			$data->getFacebook(), 
+	    			$data->getTwitter(),
+	    			$data->getId()
+	    		)
+	    	);
+
+    	}catch(Exception $e) {
+			die($e->getMessage());
+		}
+
+    }
+
+
+    public function delete($id) {
+
+    	try {
+
+	    	$sql = "DELETE FROM participante WHERE id = ? ";
+
+	    	$this->pdo->prepare($sql)->execute(
+	    		array($id)
+	    	);
+
+    	}catch(Exception $e) {
+			die($e->getMessage());
+		}
+
+    }
+
+    public function list() {
+    	try{
+    		$result = array();
+
+	    	$stm = $this->pdo->prepare("SELECT * FROM participante");
+	    	$stm->execute();
+
+	    	return $stm->fetchAll(PDO::FETCH_OBJ);
+    	}catch(Exception $e) {
+			die($e->getMessage());
+		}
+    	
+    }
+
+    public function get($id) {
+    	try{
+    		$result = array();
+
+	    	$stm = $this->pdo->prepare("SELECT * FROM participante WHERE id = ?");
+	    	$stm->execute(array($id));
+
+	    	return $stm->fetch(PDO::FETCH_OBJ);
+    	}catch(Exception $e) {
+			die($e->getMessage());
+		}
+    }
+
+
 }
 
 
--- test.php
+++ test.php
@@ -8,10 +8,23 @@ $participante = new Participante();
 $participante->setClave("1234");
 $participante->setCodigo("1152656");
 $participante->setEmail("siaweb@ufps.edu.co");
-$participante->setNombre("Carlos");
+$participante->setNombre("Lina Maria");
 $participante->setId(0);
 
-$participante->insert($participante);
+//$participante->insert($participante);
+
+//$participante->delete(3);
+
+$susy = $participante->get(2);
+
+var_dump($susy);
+
+echo("<br/>Nombre: ".$susy->nombre);
+
+foreach($participante->list() as $data) {
+	echo("<br/>Nombre: ".$data->nombre);
+}
+
 
 
 ?>
